930e1f380d491bc53f35f3300d41f847bc9c03e7
Peter Hu <peter.hu@autodesk.com>
2023-10-23 10:12:56 -0600

    update

--- runMe.sh
+++ runMe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker network create jenkins
 docker run --name jenkins-docker --rm --detach \
-  --privileged --network jenkins --network-alias docker \
+  --privileged --network jenkins --add-host=host.docker.internal:host-gateway --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
   --volume jenkins-docker-certs:/certs/client \
   --volume jenkins-data:/var/jenkins_home \
@@ -9,9 +9,9 @@ docker run --name jenkins-docker --rm --detach \
   docker:dind --storage-driver overlay2
 docker build -t my-own-jenkins:latest dockerfiles/blueoceanjenkins/.
 docker run --name jenkins-blueocean --rm --detach \
-  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --network jenkins --add-host=host.docker.internal:host-gateway --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 --publish 50000:50000 \
+  --publish 8099:8080 --publish 50000:50000 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
   my-own-jenkins:latest
